3fd02d221484f1c22f57d95e877dd8a736a3bde2
Patryk <patys.prog@gmail.com>
2016-05-29 16:20:10 +0200

    added properties to constructor

--- src/element.cpp
+++ src/element.cpp
@@ -1,7 +1,7 @@
 #include <spark/element.hpp>
 using namespace spark;
 
-IElement::IElement() : m_margin(0),m_padding(0)
+IElement::IElement() : m_margin(0),m_padding(0), m_index(0), m_width(0), m_height(0), m_visible(true)
 {
 
 }
