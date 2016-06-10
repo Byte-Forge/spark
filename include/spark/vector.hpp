ccaee6176b7db6fffbf98914a4ecc9ed1b057644
Patryk <patys.prog@gmail.com>
2016-06-10 17:02:35 +0200

    added operators to vectors

--- include/spark/vector.hpp
+++ include/spark/vector.hpp
@@ -24,6 +24,84 @@ namespace spark
 			w = c4;
 		}
 
+		bool operator ==(const vec4& v)
+		{
+			if((x == v.x) &&
+				(y == v.y) &&
+				(z == v.z) &&
+				(w == v.w))
+				return true;
+			else
+				return false;
+		}
+		
+		bool operator !=(const vec4& v)
+		{
+			if((x == v.x) &&
+				(y == v.y) &&
+				(z == v.z) &&
+				(w == v.w))
+				return false;
+			else
+				return true;
+		}
+		
+		vec4 operator +(const vec4& v)
+		{
+			return vec4(x + v.x, y + v.y, z + v.z, w + v.w);
+		}
+		
+		vec4 &operator +=(const vec4& v)
+		{
+			x += v.x;
+			y += v.y;
+			z += v.z;
+			w += v.w;
+			return *this;
+		}
+
+		vec4 operator -(const vec4& v)
+		{
+			return vec4(x - v.x, y - v.y, z - v.z, w - v.w);
+		}
+		
+		vec4 &operator -=(const vec4& v)
+		{
+			x -= v.x;
+			y -= v.y;
+			z -= v.z;
+			w -= v.w;
+			return *this;
+		}
+		
+		vec4 operator *(const vec4& v)
+		{
+			return vec4(x * v.x, y * v.y, z * v.z, w * v.w);
+		}
+		
+		vec4 &operator *=(const vec4& v)
+		{
+			x *= v.x;
+			y *= v.y;
+			z *= v.z;
+			w *= v.w;
+			return *this;
+		}
+		
+		vec4 operator /(const vec4& v)
+		{
+			return vec4(x / v.x, y / v.y, z / v.z, w / v.w);
+		}
+		
+		vec4 &operator /=(const vec4& v)
+		{
+			x /= v.x;
+			y /= v.y;
+			z /= v.z;
+			w /= v.w;
+			return *this;
+		}
+		
 		union
 		{
 			T xyzw[4];
@@ -51,6 +129,72 @@ namespace spark
 		{
 			x = c1;
 			y = c2;
+		}
+		
+		bool operator ==(const vec2& v)
+		{
+			if((x == v.x) &&
+				(y == v.y))
+				return true;
+			else
+				return false;
+		}
+		
+		bool operator !=(const vec2& v)
+		{
+			if((x == v.x) &&
+				(y == v.y))
+				return false;
+			else
+				return true;
+		}
+		
+		vec2 operator +(const vec2& v)
+		{
+			return vec2(x + v.x, y + v.y);
+		}
+		
+		vec2 &operator +=(const vec2& v)
+		{
+			x += v.x;
+			y += v.y;
+			return *this;
+		}
+
+		vec2 operator -(const vec2& v)
+		{
+			return vec2(x - v.x, y - v.y);
+		}
+		
+		vec2 &operator -=(const vec2& v)
+		{
+			x -= v.x;
+			y -= v.y;
+			return *this;
+		}
+		
+		vec2 operator *(const vec2& v)
+		{
+			return vec2(x * v.x, y * v.y);
+		}
+		
+		vec2 &operator *=(const vec2& v)
+		{
+			x *= v.x;
+			y *= v.y;
+			return *this;
+		}
+		
+		vec2 operator /(const vec2& v)
+		{
+			return vec2(x / v.x, y / v.y);
+		}
+		
+		vec2 &operator /=(const vec2& v)
+		{
+			x /= v.x;
+			y /= v.y;
+			return *this;
 		}
 
 		union
