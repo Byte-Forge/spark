1d69d785c15ebe80f109a544f797a8db3269f176
feliwir <stephan.vedder@gmail.com>
2016-08-17 10:14:23 +0200

    Fix scriptengine

--- include/spark/scriptengine.hpp
+++ include/spark/scriptengine.hpp
@@ -12,10 +12,11 @@ namespace spark
 	class IScriptEngine
 	{
 	public:
+		IScriptEngine(const std::string& lang) : m_lang(lang) {};
 		virtual void ExecuteString(const std::string& script) = 0;
 		virtual void ExecuteFile(const std::string& file) = 0;
 		inline const std::string GetLang() {return m_lang;}
-	protected:
+	private:
 		const std::string m_lang;
 		
 	};
